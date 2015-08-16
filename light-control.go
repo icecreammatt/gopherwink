3960dfe2dc046d3892d4a8549daef1a105f8b79b
Matt Carrier <icecreammatt@gmail.com>
2015-08-16 13:23:06 -0700

    Add route for setting LED colors

--- light-control.go
+++ light-control.go
@@ -1,15 +1,24 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"net/http"
 	"os/exec"
+	"strconv"
 )
 
 type Light struct {
-	Id     uint
-	Value  uint
-	Active bool
+	Id     uint `json: "id"`
+	Value  uint `json: "value"`
+	Active bool `json: "active"`
+}
+
+type RGB struct {
+	Red   int `json: "red"`
+	Green int `json: "green"`
+	Blue  int `json: "blue"`
 }
 
 var ServerName = "http://192.168.1.11:5000"
@@ -41,7 +50,37 @@ func HandleLight(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+func HandleLED(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Allow-Control-Allow-Origin", ServerName)
+	var colors RGB
+	body, err := ioutil.ReadAll(r.Body)
+	err = json.Unmarshal(body, &colors)
+	if !logError(err) {
+		args := []string{strconv.Itoa(colors.Red), strconv.Itoa(colors.Green), strconv.Itoa(colors.Blue)}
+		cmd := exec.Command("/usr/sbin/set_rgb", args...)
+		err = cmd.Run()
+		if !logError(err) {
+			status := 200
+			fmt.Fprintf(w, "Success: %d", status)
+		} else {
+			fmt.Fprintf(w, "Error: %s", err.Error())
+		}
+	}
+}
+
 func main() {
 	http.HandleFunc("/light", HandleLight)
+	http.HandleFunc("/led", HandleLED)
 	http.ListenAndServe(":5000", nil)
+	fmt.Println("Listening on port 5000")
+}
+
+func logError(err error) (isError bool) {
+	if err != nil {
+		fmt.Println("Error:", err.Error())
+		isError = true
+	} else {
+		isError = false
+	}
+	return
 }
