cead4d29610eabc8eafae23b8145899c36c030a7
Matt Carrier <icecreammatt@gmail.com>
2015-09-11 23:02:28 -0700

    Sample daylight auto on off test

--- gopherwink.go
+++ gopherwink.go
@@ -2,11 +2,12 @@ package main
 
 import (
 	"fmt"
+	"github.com/icecreammatt/gopherwink/scheduler"
 	"github.com/rs/cors"
 	"net/http"
 )
 
-var port string = "5000"
+var port string = "5001"
 
 func main() {
 	c := cors.New(cors.Options{
@@ -17,5 +18,6 @@ func main() {
 	})
 	router := NewRouter()
 	fmt.Println("Listening on port", port)
+	scheduler.Start(60 * 1000)
 	http.ListenAndServe(":"+port, c.Handler(router))
 }
