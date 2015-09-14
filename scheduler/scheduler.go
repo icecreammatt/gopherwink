9af230c11d46e264520a218b8f227493585926f3
Matt Carrier <icecreammatt@gmail.com>
2015-09-14 00:22:53 -0700

    Handle list of lights to turn on and off with sunrise & sunset

--- scheduler/scheduler.go
+++ scheduler/scheduler.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"github.com/jonaz/astrotime"
 	"os/exec"
+	"strconv"
 	"time"
 )
 
@@ -12,9 +13,9 @@ const LONGITUDE = float64(-122.816380)
 
 func isSunset() bool {
 	t := astrotime.NextSunset(time.Now(), LATITUDE, LONGITUDE)
-	tzname, _ := t.Zone()
-	fmt.Println(tzname)
-	fmt.Printf("The next sunrise is %d:%02d %s on %d/%d/%d.\n", t.Hour(), t.Minute(), tzname, t.Month(), t.Day(), t.Year())
+	// tzname, _ := t.Zone()
+	// fmt.Println(tzname)
+	// fmt.Printf("The next sunrise is %d:%02d %s on %d/%d/%d.\n", t.Hour(), t.Minute(), tzname, t.Month(), t.Day(), t.Year())
 	if t.Hour() == time.Now().Hour() && t.Minute() == time.Now().Minute() {
 		return true
 	}
@@ -24,9 +25,10 @@ func isSunset() bool {
 func isSunrise() bool {
 	t := astrotime.NextSunrise(time.Now(), LATITUDE, LONGITUDE)
 	tzname, _ := t.Zone()
-	fmt.Println(tzname)
-	fmt.Printf("The next sunrise is %d:%02d %s on %d/%d/%d.\n", t.Hour(), t.Minute(), tzname, t.Month(), t.Day(), t.Year())
+	// fmt.Println(tzname)
+	// fmt.Printf("The next sunrise is %d:%02d %s on %d/%d/%d.\n", t.Hour()-6, t.Minute()-25, tzname, t.Month(), t.Day(), t.Year())
 	if t.Hour() == time.Now().Hour() && t.Minute() == time.Now().Minute() {
+		fmt.Printf("The sunrise is %d:%02d %s on %d/%d/%d.\n", t.Hour(), t.Minute(), tzname, t.Month(), t.Day(), t.Year())
 		return true
 	}
 	return false
@@ -35,17 +37,27 @@ func isSunrise() bool {
 func Start(intervalInSeconds int) {
 	ticker := time.NewTicker(time.Millisecond * time.Duration(intervalInSeconds))
 	go func() {
+		// Hard code lights to change for now
+		lights := [2]int{1, 8}
 		for t := range ticker.C {
+			var state string
+			var changeLights bool
 			if isSunset() {
 				fmt.Println("Sunset", t)
-				args := []string{"-u", "-m" + "1", "-t1", "-v" + "ON"}
-				cmd := exec.Command("/usr/sbin/aprontest", args...)
-				cmd.Run()
+				state = "ON"
+				changeLights = true
 			} else if isSunrise() {
 				fmt.Println("Sunrise", t)
-				args := []string{"-u", "-m" + "1", "-t1", "-v" + "OFF"}
-				cmd := exec.Command("/usr/sbin/aprontest", args...)
-				cmd.Run()
+				state = "OFF"
+				changeLights = true
+			}
+			if changeLights == true {
+				fmt.Println("Updating lights")
+				for _, light := range lights {
+					args := []string{"-u", "-m" + strconv.Itoa(light), "-t1", "-v" + state}
+					cmd := exec.Command("/usr/sbin/aprontest", args...)
+					cmd.Run()
+				}
 			}
 		}
 	}()
