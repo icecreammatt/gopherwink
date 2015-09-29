d09d4f30fc3eb37839eae9d0886c36426623a38c
Matt Carrier <icecreammatt@gmail.com>
2015-09-28 21:07:54 -0700

    Experiment with database

--- gopherwink.go
+++ gopherwink.go
@@ -4,12 +4,24 @@ import (
 	"fmt"
 	"github.com/icecreammatt/gopherwink/scheduler"
 	"github.com/rs/cors"
+	"github.com/syndtr/goleveldb/leveldb"
 	"net/http"
 )
 
 var port string = "5001"
 
 func main() {
+
+	db, err := leveldb.OpenFile("devices.db", nil)
+	defer db.Close()
+
+	data, err := db.Get([]byte("key"), nil)
+	if err != nil {
+		fmt.Println("error fetching data", err.Error())
+	}
+	fmt.Println(string(data))
+	err = db.Put([]byte("key"), []byte("here is another sample"), nil)
+
 	c := cors.New(cors.Options{
 		AllowedOrigins:     []string{"*"},
 		AllowedMethods:     []string{"GET", "POST", "OPTIONS", "PUT", "DELETE"},
