7c3effcdf09645d667ee409a456700b15ec9af4d
Matt Carrier <icecreammatt@gmail.com>
2015-09-29 23:05:18 -0700

    Print sensor name and state when triggered in slack message

--- scripts/monitor.sh
+++ scripts/monitor.sh
@@ -4,9 +4,25 @@ tail -Fn0 /var/log/messages | while read line ; do
     echo "$line" | grep "Received zwave General event UPDATE callback for node"
     if [ $? = 0 ]
     then
-    # Actions
     res=$(echo "Logging: $line" | grep -Eo "node [0-9]")
-    message="`date` zwave device triggered: $res"
+    idx=$(echo $res | grep -Eo "[0-9]")
+
+    device=$(sqlite3 /database/apron.db "select masterId, userName from zwaveDevice, masterDevice where nodeId = $idx and deviceId=masterId")
+    masterid=`echo $device | cut -d \| -f 1`
+    userName=`echo $device | cut -d \| -f 2`
+
+    data=$(aprontest -m $masterid -l)
+    newState=$(echo "$data" | grep -Ew "On_Off" | awk '{ print $9 }')
+
+    switchState=""
+    if [ $newState = "TRUE" ]
+    then
+    switchState="OPENED"
+    else
+    switchState="CLOSED"
+    fi
+
+    message="`date` $userName = $switchState"
     /root/monitor/slack-notify.sh ${message}
     fi
 done
